d514b1f438756fbda1302d524e5e5e0cb163593e
Siddharth Gosavi <2014370@corp.trizetto.com>
2023-07-17 17:29:16 +0530

    User Profile find-update/delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,4 +107,66 @@ router.post(
   }
 );
 
+//@route    Get api/profile/
+//@desc     GetAllProfiles
+//@access   Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    Get api/profile/user/:user_id
+//@desc     Get Profile by user ID
+//@access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    DELETE api/profile/
+//@desc     Delete Profile,USER & Posts
+//@access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo -remove users Posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({
+      user: req.user.id,
+    });
+
+    // Remove user
+    await User.findOneAndRemove({
+      _id: req.user.id,
+    });
+
+    res.json({ msg: 'User Deleted' });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
